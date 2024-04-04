0e3bcb736453936ed4ed512cd826f7490cdb6b9b
Pengpa0012 <peng.ilikha@gmail.com>
2024-04-04 20:26:06 +0800

    update asset, added controls

--- car/script.js
+++ car/script.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 2000)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -12,11 +12,10 @@ const light = new THREE.AmbientLight( 0xffffff, 20 )
 scene.add( light )
 
 
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls( camera, renderer.domElement )
 
-//controls.update() must be called after any manual changes to the camera's transform
-camera.position.set( 0, 20, 100 );
-controls.update();
+camera.position.set( 0, 20, 100 )
+controls.update()
 
 camera.position.z = 8
 document.body.appendChild(renderer.domElement)
@@ -25,7 +24,7 @@ document.body.appendChild(renderer.domElement)
 
 const loader = new GLTFLoader()
 loader.load(
-	'assets/car-1/scene.gltf',
+	'assets/car-3/scene.gltf',
 	function ( gltf ) {
 		scene.add( gltf.scene )
 		gltf.animations // Array<THREE.AnimationClip>
@@ -48,5 +47,11 @@ function animate() {
 	controls.update()
 	renderer.render(scene, camera)
 }
+
+window.addEventListener("resize", () => {
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(window.innerWidth, window.innerHeight)
+})
 
 animate()
