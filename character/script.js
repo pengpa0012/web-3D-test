8d464cfed6134269c4e5b5e6e656a66937bf9f78
Pengpa0012 <peng.ilikha@gmail.com>
2024-04-06 20:42:56 +0800

    added model, animation

--- character/script.js
+++ character/script.js
@@ -1,12 +1,24 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+let action, 
+		mixer, 
+		clock = new THREE.Clock(), 
+		isMoving, 
+		animations
 
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xffffff)
 const camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 2000)
+const loader = new GLTFLoader()
 
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
+
+const light = new THREE.AmbientLight(0xffffff, 5)
+scene.add(light)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
@@ -15,9 +27,34 @@ controls.update()
 
 document.body.appendChild(renderer.domElement)
 
+loader.load(
+	`assets/scene.gltf`,
+	function (gltf) {
+		const clip = THREE.AnimationClip.findByName(gltf.animations, 'Walk')
+		mixer = new THREE.AnimationMixer(gltf.scene)
+		action = mixer.clipAction(clip)
+		action.play()
+		animations = gltf.animations
+		scene.add(gltf.scene)
+		gltf.animations // Array<THREE.AnimationClip>
+		gltf.scene // THREE.Group
+		gltf.scenes // Array<THREE.Group>
+		gltf.cameras // Array<THREE.Camera>
+		gltf.asset // Object
+	},
+	function (xhr) {
+		console.log( (xhr.loaded / xhr.total * 100) + '% loaded')
+	},
+	function (error) {
+		console.log(error)
+	}
+)
+
 function animate() {
 	requestAnimationFrame(animate)
 	controls.update()
+	let delta = clock.getDelta()
+	if (mixer && isMoving) mixer.update( delta )
 	renderer.render(scene, camera)
 }
 
@@ -25,6 +62,16 @@ window.addEventListener("resize", () => {
 	camera.aspect = window.innerWidth / window.innerHeight
 	camera.updateProjectionMatrix()
 	renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+window.addEventListener("keydown", e => {
+	if(e.key == "w" || e.key == "a" || e.key == "s" || e.key == "d") {
+		isMoving = true
+	}
+})
+
+window.addEventListener("keyup", () => {
+	isMoving = false
 })
 
 animate()
